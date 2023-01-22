f07cee03721a87543e15c8feec4512061f549260
cassandra-mn <arthenciac@gmail.com>
2023-01-22 18:11:01 -0300

    feat: create user model

---
+++ src/main/java/tweteroo/api/model/User.java
@@ -0,0 +1,32 @@
+package tweteroo.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import tweteroo.api.dto.UserDTO;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class User {
+    public User(UserDTO dto) {
+        this.username = dto.username();
+        this.avatar = dto.avatar();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String username;
+
+    @Column(length = 500, nullable = false)
+    private String avatar;
+}
