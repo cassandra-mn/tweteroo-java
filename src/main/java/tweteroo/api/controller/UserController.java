6b622f8e78f607bdb164abdb9d6ae9f8604cc25b
cassandra-mn <arthenciac@gmail.com>
2023-01-22 18:09:35 -0300

    feat: create user controller

---
+++ src/main/java/tweteroo/api/controller/UserController.java
@@ -0,0 +1,44 @@
+package tweteroo.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import tweteroo.api.dto.UserDTO;
+import tweteroo.api.model.User;
+import tweteroo.api.service.UserService;
+
+@RestController
+@RequestMapping("/sign-up")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public List<User> findAll() {
+        return service.listAll();
+    }
+
+    @PostMapping
+    public void create(@RequestBody UserDTO user) {
+        service.create(user);
+    }
+
+    @PutMapping("/{id}") 
+    public void update(@RequestBody UserDTO user, @PathVariable Long id) {
+        service.update(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
