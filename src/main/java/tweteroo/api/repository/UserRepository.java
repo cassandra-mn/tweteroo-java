091a11c34471479edac76f203334d56098fab4c8
cassandra-mn <arthenciac@gmail.com>
2023-01-22 18:10:12 -0300

    feat: create user repository

---
+++ src/main/java/tweteroo/api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package tweteroo.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tweteroo.api.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
