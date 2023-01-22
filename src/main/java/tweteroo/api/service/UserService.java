adc264de9ce3db68143dcd80cba4659bf1e0eecd
cassandra-mn <arthenciac@gmail.com>
2023-01-22 18:10:34 -0300

    feat: create user service

---
+++ src/main/java/tweteroo/api/service/UserService.java
@@ -0,0 +1,41 @@
+package tweteroo.api.service;
+
+import jakarta.validation.Valid;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tweteroo.api.dto.UserDTO;
+import tweteroo.api.model.User;
+import tweteroo.api.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> listAll() {
+        return repository.findAll();
+    }
+
+    public User create(@Valid UserDTO dto) {
+        return repository.save(new User(dto));
+    }
+
+    public void update(Long id, UserDTO dto) {
+        repository.findById(id).map(user -> {
+            if (dto.username().length() > 0)
+                user.setUsername(dto.username());
+
+            if (dto.avatar().length() > 0)
+                user.setAvatar(dto.avatar());
+
+            return repository.save(user);
+        });
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
