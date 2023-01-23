b00d0e7149af65e11511aae44f8ff54a5d23417e
cassandra.mn <arthenciac@gmail.com>
2023-01-23 12:19:54 -0300

    feat: create tweet controller

---
+++ src/main/java/tweteroo/api/controller/TweetController.java
@@ -0,0 +1,31 @@
+package tweteroo.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import tweteroo.api.dto.TweetDTO;
+import tweteroo.api.model.Tweet;
+import tweteroo.api.service.TweetService;
+
+@RestController
+@RequestMapping("/tweets")
+public class TweetController {
+    @Autowired
+    private TweetService service;
+
+    @GetMapping
+    public Page<Tweet> findAllPage(@RequestParam(value = "page", required = false, defaultValue = "0") Integer page) {
+        return service.listAll(page);
+    }
+    
+    @PostMapping
+    public String create(@RequestBody TweetDTO tweet) {
+        return service.create(tweet);
+    }
+}
