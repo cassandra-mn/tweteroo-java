ac88d0b998e46a349ba666d6c4de8fbdb7fde0b4
cassandra.mn <arthenciac@gmail.com>
2023-01-23 12:21:05 -0300

    feat: create tweet service

--- src/main/java/tweteroo/api/repository/UserRepository.java
+++ src/main/java/tweteroo/api/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tweteroo.api.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    User findByUsername(String username);
 }
---
+++ src/main/java/tweteroo/api/service/TweetService.java
@@ -0,0 +1,35 @@
+package tweteroo.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import jakarta.validation.Valid;
+import tweteroo.api.dto.TweetDTO;
+import tweteroo.api.model.Tweet;
+import tweteroo.api.repository.TweetRepository;
+import tweteroo.api.repository.UserRepository;
+
+@Service
+public class TweetService {
+    @Autowired
+    private TweetRepository repository;
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    public Page<Tweet> listAll(int page) {
+        PageRequest pageable = PageRequest.of(0, 5).withPage(page);
+
+        return repository.findAll(pageable);
+    }
+
+    public String create(@Valid TweetDTO dto) {
+        String avatar = userRepository.findByUsername(dto.username()).getAvatar();
+
+        repository.save(new Tweet(dto, avatar));
+
+        return "OK";
+    }
+}
